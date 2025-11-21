0931182af502c222952b970695f6d48637f14a24
minim22 <cutesing@naver.com>
2025-11-21 13:31:52 +0900

    feat: 초기 fix

---
+++ src/main/java/com/custom/recommend_activity_service/RecommendActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.custom.recommend_activity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendActivityServiceApplication.class, args);
+	}
+
+}
